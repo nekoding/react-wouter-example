30946c5bdc91a78717f984d71136577dcff9360e
Enggar Tivandi <enggartivandi@outlook.com>
2024-01-12 08:48:05 +0700

    chore: update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_PATH || "/",
+  base: process.env.BASE_PATH || "/react-wouter",
   plugins: [react()],
 });
